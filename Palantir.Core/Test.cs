eccba4ef597ee2f7170a39c33c81d5efeef2198f
Carlos Lubián Fernández <carlos.lubianfernandez@arcelormittal.com>
2025-06-16 16:57:30 +0200

    Awaiting NuGet listing

---
+++ Palantir.Core/Test.cs
@@ -0,0 +1,17 @@
+﻿using Armali.Horizon;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Palantir.Core
+{
+    internal class Test
+    {
+        internal void RunTest()
+        {
+            var horizon = new Horizon(new string[] { "arg1", "arg2" });
+        }
+    }
+}
---
+++ Palantir.IMES/Program.cs
@@ -0,0 +1,12 @@
+﻿using Armali.Horizon;
+
+namespace Palantir.IMES
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var horizon = new Horizon(args);
+        }
+    }
+}
